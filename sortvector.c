9a62020909bbd011c5f27139c3bb1648a1f10213
Mihai Iliescu <iliescufm@yahoo.com>
2023-11-21 08:03:23 +0200

    example how to use top index for displaying sotted matrix

--- sortvector.c
+++ sortvector.c
@@ -1,3 +1,11 @@
+/*
+sortvector.c
+v 1.0
+
+gcc sortvector.c Radix.c
+
+*/
+
 #include <stdio.h>
 #include "Radix.h"
 #include <stdlib.h>
@@ -13,23 +21,35 @@ void print(int arr[], int n)
 
 int main()
 {
+
+    printf("Example 1\n");
+
 	int arr[] = { 543, 986, 217, 765, 329, 12, 114, 999, 5 };
+  	int vct[] = { 543, 986, 217, 765, 329, 12, 114, 999, 5 };
+
     int *idx;
 	int n = sizeof(arr) / sizeof(arr[0]);
 
+    printf("Example 2\n");
    	print(arr, n);   
     idx = sortVector(arr,n);
 	print(arr, n);
     print(idx, n);
+    printf("use top index\n");
+    for (int i = 0; i < n;  i++){
+        printf(" %3d",vct[idx[i]]);
+    }
+    printf("\n");
 
-    n=3;
-    int* v = malloc(n * sizeof(int));
+    printf("Example 2\n");
+    int* v = malloc(3 * sizeof(int));
     v[0] = 18;
     v[1] = 21;
     v[2] = 10;
-    idx = sortVector(v,n);
-	print(v, n);
-    print(idx, n);
+    idx = sortVector(v,3);
+	print(v, 3);
+    print(idx, 3);
+
 
 	return 0;
 }
