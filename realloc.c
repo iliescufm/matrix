91af4f15a2b5d5df3bdddbf70c04973a1c403a6f
Mihai Iliescu <iliescufm@yahoo.com>
2023-11-22 08:13:28 +0200

    Understanding realloc

--- realloc.c
+++ realloc.c
@@ -1,57 +1,38 @@
+/*
+realloc.c
+v1.0
+
+gcc realloc.c Matrix.c 
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "Matrix.h"
 
 int main() {
-    // Initial size of the vector
-    int initialSize = 5;
 
-    // Allocate memory for the vector
-    int *vector = (int *)malloc(initialSize * sizeof(int));
-    if (vector == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        return 1;
-    }
+    int k;
+   	char m1[] = "11 12 13 14";
+	int *a = stringToVector(m1, &k);
 
-    // Populate the vector with some values
-    for (int i = 0; i < initialSize; i++) {
-        vector[i] = i * 2;
-    }
+	printVectorAddr(a,k);
 
-    // Print the original vector
-    printf("Original Vector:\n");
-    for (int i = 0; i < initialSize; i++) {
-        printf("%d [%X] ", vector[i], vector+i);
-    }
-    printf("\n");
+	int *b;
+	b = realloc(a, (10) * sizeof(int));
+	//allocate memory at address b;
+	//copies in at the addres b elements from address a;
+	//frees the memory used by a
+	//adress of a should no loger used;
 
-    // Resize the vector using realloc
-    int newSize = 10;
-    int *resizedVector = (int *)realloc(vector, newSize * sizeof(int));
+	printVectorAddr(b,10);
+	printVectorAddr(a,10);
 
-    if (resizedVector == NULL) {
-        fprintf(stderr, "Memory reallocation failed\n");
-        free(vector);  // Release the original memory
-        return 1;
-    }
+	a = b;
+	printVectorAddr(a,10);
 
-    // If realloc was successful, use the resizedVector pointer 
-    vector = resizedVector;
+    b = resizeVector(b,10,12);
+   	printVectorAddr(b,12);
 
-    // Populate the additional elements in the resized vector
-    for (int i = initialSize; i < newSize; i++) {
-        vector[i] = i * 2;
-    }
 
-    // Print the resized vector
-    printf("Resized Vector:\n");
-    for (int i = 0; i < newSize; i++) {
-        printf("%d [%x] ", vector[i], &vector[i]);
-        
-    }
-    printf("\n");
-
-    // Free the memory when you're done using it
-    free(vector);
-
-    return 0;
 }
