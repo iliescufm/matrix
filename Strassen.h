fe816309f9819265cb835cd97c4f9c3eac9f3198
Mihai Iliescu <iliescufm@yahoo.com>
2023-11-26 06:12:16 +0200

    remove Strassen

--- Strassen.h
+++
@@ -1,5 +0,0 @@
-// Functie pentru a inmulti doua matrice folosind algoritmul lui Strassen
-void strassenMultiply(int **A, int **B, int **C, int size);
-int** strassen(int **A, int la, int ca, int **B, int lb, int lc);
-
- 
